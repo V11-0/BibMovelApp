897ef6b022651929ea7391cc0c6bdc7957f3fc8b
Vinícius Breno Bruscagini <vinibrenobr11@hotmail.com>
2018-08-14 21:10:59 -0300

    Initial Commit

---
+++ app/src/main/java/com/bibmovel/client/LoginActivity.java
@@ -0,0 +1,142 @@
+package com.bibmovel.client;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private GoogleSignInClient mGoogleSignInClient;
+
+    private EditText edt_user;
+    private EditText edt_pass;
+
+    private static final int RC_SIGN_IN = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.g_sign_in).setOnClickListener(v -> signIn());
+
+        edt_user = findViewById(R.id.edt_user);
+        edt_pass = findViewById(R.id.edt_pass);
+
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user = edt_user.getText().toString();
+                String pass = edt_pass.getText().toString();
+
+                // TODO: 11/08/18 Verificação dos dados
+
+                logOn(user, pass);
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            GoogleSignInAccount account = task.getResult();
+            logOn(account);
+        }
+
+    }
+
+    private void logOn(String user, String pass) {
+
+        if (user.equals("foo") && pass.equals("bar")) {
+            // TODO: 11/08/18 Pegar credenciais do servidor usando asynctask
+            Intent it = new Intent(this, MainActivity.class);
+            it.putExtra("user", user);
+            it.putExtra("email", "email");
+
+            startActivity(it);
+
+        } else {
+            Snackbar.make(edt_user, "Credenciais incorretas"
+                    , Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void logOn(GoogleSignInAccount account) {
+
+        Intent it = new Intent(this, MainActivity.class);
+        it.putExtra("google_account", account);
+
+        startActivity(it);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Verifica se usuário já está logado com conta google
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null)
+            logOn(account);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.item_register:
+                // TODO: 10/08/18 Cadastro por dialog
+                AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+                dlg.setView(R.layout.dialog_register)
+                        .setTitle("Cadastrar")
+                        .setPositiveButton("Continuar", (dialog, which) -> {
+
+                }).create().show();
+                break;
+            case R.id.item_settings:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
