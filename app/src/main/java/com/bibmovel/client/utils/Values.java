4bc866e58f99572f543252fdde3d904bdb1b3a0c
vinibrenobr11 <vinibrenobr11@hotmail.com>
2018-09-11 10:51:03 -0300

    Added Splash Activity

---
+++ app/src/main/java/com/bibmovel/client/utils/Values.java
@@ -0,0 +1,13 @@
+package com.bibmovel.client.utils;
+
+/**
+ * Created by vinibrenobr11 on 11/09/2018 at 10:26
+ */
+public class Values {
+
+    private static final String PREFS_LOGIN = "";
+
+    public static String getPrefsLogin() {
+        return PREFS_LOGIN;
+    }
+}
