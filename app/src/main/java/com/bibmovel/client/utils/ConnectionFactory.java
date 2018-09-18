21288927ce070ed946b6076f4a91e0ef3419916a
vinibrenobr11 <vinibrenobr11@hotmail.com>
2018-09-18 10:29:14 -0300

    Started MySQL and Samba implementations

---
+++ app/src/main/java/com/bibmovel/client/utils/ConnectionFactory.java
@@ -0,0 +1,30 @@
+package com.bibmovel.client.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by vinibrenobr11 on 18/09/2018 at 10:11
+ */
+public abstract class ConnectionFactory {
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+
+        if (connection.isValid(10))
+            return connection;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        connection = DriverManager.getConnection("jdbc:mysql://192.168.0.100:3306/Bib"
+                , "root", "1234");
+
+        return connection;
+    }
+}
