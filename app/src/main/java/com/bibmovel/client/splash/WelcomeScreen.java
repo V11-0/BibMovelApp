1873bec5bda03c818e2d74a1e67a7eb2e1dc024e
mvbertos <M@t1029384756>
2018-11-06 09:39:38 -0200

    +Layout Changes

--- app/src/main/java/com/bibmovel/client/splash/WelcomeScreen.java
+++ app/src/main/java/com/bibmovel/client/splash/WelcomeScreen.java
@@ -18,10 +18,11 @@ public class WelcomeScreen extends WelcomeActivity {
         return new WelcomeConfiguration.Builder(this)
                 .defaultBackgroundColor(R.color.welcome_background1)
 
-                .page(new TitlePage(0, "Title"))
-                .page(new BasicPage(0, "Header", "More text.")
+                .page(new TitlePage(0, "Oque é o aplicativo"))
+                .page(new BasicPage(0, "Header", "BibMovel é um aplicativo de gerenciamento de livros" +
+                        " que te possibilita compartilhar e baixar sem poblemas.")
                         .background(R.color.welcome_background2))
-                .page(new BasicPage(0, "Lorem ipsum", "dolor sit amet.")
+                .page(new BasicPage(0, "Header", "Otilize-o com responsabilidades.")
                         .background(R.color.welcome_background3))
 
                 .swipeToDismiss(true)
